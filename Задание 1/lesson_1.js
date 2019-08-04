dacfba40427df160a9086c5116b359025ee6de25
Yurback <ryv1983@mail.ru>
2019-08-04 23:10:38 +0300

    Lesson_2 done

--- Задание 1/lesson_1.js
+++ Задание 1/lesson_1.js
@@ -1,30 +1,99 @@
 'use strict';
 
-var money = prompt('Ваш бюджет за месяц', '__руб.');
-var time = prompt('Введите дату в формате YYYY-MM-DD','');
+var array2 = [];
+array2[0] = 200; array2[1] = 300; array2[2] = 400;
+array2.push(500);
+console.log(array2);
+console.log(array2.pop());
+console.log(array2);
+console.log(array2.shift());
+console.log(array2);
 
-var Qst_1 = prompt('Введите обязательную статью в этом месяце','');
-var Qst_1_1 = prompt('Во сколько обойдется','');
+var samay_bolshaya_peremennaya
 
-var Qst_2 = prompt('Введите обязательную статью в этом месяце','');
-var Qst_2_1 = prompt('Во сколько обойдется','');
+var obj2 = {};
+obj2.name = "DIMA";
+console.log(obj2.name[1-]);
+(obj2.name === "DIMA")? alert("Привет Дима!") : alert("это не Дима");
 
-var appData = {
-    budzhet:  money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: false
+
+var obj1 = {
+    words: ["Привет!", ],
+    name: "VASIY",
+    profession: "worker",
+    ages: 25,
+    to_remont: function() {
+        
+    },
+    to_drive: function() {
+      
+    }
+};
+
+// while (i>10) {
+
+
+
+    
+// }
+
+// for(var i = 0; i < 10; ++i) {
+//     alert("Привет мир!")
+// }
+
+// var numb = Infinit;
+
+
+console.log(Math.random());
+
+// var something;
+
+
+var array = ["aplle", 4, 10]; // этот код для примера
+
+// console.log(numb);
+foo();
+
+
+
+
+function foo() {
+    if (array[1] == "5") {
+        alert(array[0]);
+    } else return;
+
+    prompt('Введите обязательную статью в этом месяце','');
+
 }
-appData.expenses[Qst_1] = Qst_1_1;
-appData.expenses[Qst_2] = Qst_2_1;
 
 
 
-alert("Ваш бюджет на один день составил: " + (+appData.budzhet)/30 + " руб.");
 
-console.log(appData);
+// var money = prompt('Ваш бюджет за месяц', '__руб.');
+// var time = prompt('Введите дату в формате YYYY-MM-DD','');
+
+// var Qst_1 = prompt('Введите обязательную статью в этом месяце','');
+// var Qst_1_1 = prompt('Во сколько обойдется','');
+
+// var Qst_2 = prompt('Введите обязательную статью в этом месяце','');
+// var Qst_2_1 = prompt('Во сколько обойдется','');
+
+// var appData = {
+//     budzhet:  money,
+//     timeData: time,
+//     expenses: {},
+//     optionalExpenses: {},
+//     income: [],
+//     savings: false
+// }
+// appData.expenses[Qst_1] = Qst_1_1;
+// appData.expenses[Qst_2] = Qst_2_1;
+
+
+
+// alert("Ваш бюджет на один день составил: " + (+appData.budzhet)/30 + " руб.");
+
+// console.log(appData);
 
 /* 1 вопрос - 7 типов данных;
    2 вопрос - использовать команду console.log();
