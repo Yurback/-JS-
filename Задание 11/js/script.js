49b567ab0efa45739b0361c35a9812d9bdf989ce
Yurback <ryv1983@mail.ru>
2019-09-08 00:04:19 +0300

    Задание 11 выполнено

---
+++ Задание 11/js/script.js
@@ -0,0 +1,116 @@
+window.addEventListener('DOMContentLoaded', function(){
+
+    'use strict';
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for( let i = a; i<tabContent.length; i++){
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i<tab.length; i++){
+                 if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                 }
+            }
+        }
+    })
+
+    let deadline = "2019-09-09 23:00:00";
+    console.log(new Date(Date.parse(deadline)));
+    console.log(new Date(Date.parse(deadline)-1000*60*60*3));
+    console.log(Date.parse(new Date()));
+    console.log(new Date());
+
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - new Date(),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor(t/(1000*60*60));
+
+        if (seconds < 10) seconds = "0"+seconds;
+        if (minutes < 10) minutes = "0"+minutes;
+        if (hours < 10) hours = "0"+hours;
+
+        return {
+            total: t,
+            seconds: seconds,
+            minutes: minutes,
+            hours: hours
+        }      
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector(".hours"),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector(".seconds"),
+            timerInterval = setInterval(updateClock, 1000);
+        
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if(t.total <= 0) {
+                clearInterval(timerInterval);
+                hours.textContent = "00";
+                minutes.textContent = "00";
+                seconds.textContent = "00";
+            }   
+        }
+    }
+
+    setClock('timer', deadline)
+
+// Modal
+
+let more = document.querySelector(".more"),
+    overlay = document.querySelector(".overlay"),
+    close = document.querySelector(".popup-close");
+
+function hiddeBody() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash')
+        document.body.style.overflow = 'hidden';
+}
+
+more.addEventListener('click', hiddeBody)
+
+close.addEventListener('click', function(){
+    overlay.style.display = 'none';
+    this.classList.remove('more-splash');
+    document.body.style.overflow = '';
+})
+
+// Задание 1
+
+let tabShow_more = document.getElementsByClassName("description-btn");
+
+for (var i = 0; i<tabShow_more.length; i++){
+    tabShow_more[i].addEventListener('click', hiddeBody);
+}
+
+})
+
---
+++ Задание 11/js/script2.js
@@ -0,0 +1,8 @@
+//Задание 2
+
+/* <input id="age" value="30">
+let age = document.getElementById('age');
+function showUser(surname, name) {
+	alert("Пользователь " + surname + " " + name + ", его возраст " + age.value);
+}
+showUser(surname, name); */
